3a56456eb2bac510516f6008f32b4e426cf97e05
ROO <72269977+ROIZENGH@users.noreply.github.com>
2021-07-30 18:32:27 -0500

    tutorial #18

--- src/index.js
+++ src/index.js
@@ -6,24 +6,38 @@ import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import { reduxFirestore, getFirestore } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import thunk from 'redux-thunk';
+import { createFirestoreInstance, reduxFirestore, getFirestore } from 'redux-firestore';
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
 import fbConfig from "./config/fbConfig";
+import firebase from "firebase/app";
 
 const store = createStore(rootReducer, 
   compose(
-    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig)
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(firebase, fbConfig),
   )
 );
+
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true
+}
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <App />
+      </ReactReduxFirebaseProvider>
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -10,12 +10,13 @@ const projectReducer = (state = initialState, action) => {
     switch(action.type) {
         case "CREATE_PROJECT": 
             console.log("created project", action.project)
-            break;
+            return state;
+        case "CREATE_PROJECT_ERROR":
+            console.log("create project error", action.err)
+            return state;
         default: 
-            console.log("incorrect action type")
-            break;
+            return state;
     }
-    return state;
 }
 
 export default projectReducer;
