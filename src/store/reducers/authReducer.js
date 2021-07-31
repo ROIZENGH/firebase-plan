d6bcf988926e7e2a4b82dfd110e40a04fc4315e4
ROO <72269977+ROIZENGH@users.noreply.github.com>
2021-07-31 17:06:38 -0500

    Tutorial #24

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -3,7 +3,27 @@ const initialState = {
 }
 
 const authReducer = (state = initialState, action) => {
-    return state;
+    switch(action.type){
+        case "LOGIN_ERROR":
+            console.log("login error")
+            return { 
+                ...state, 
+                authError: "The email or password is incorrect. Please try again."
+            }
+        case "LOGIN_SUCCESS":
+            console.log("login success")
+            return {
+                ...state,
+                authError: null,
+            }
+        case "SIGNOUT_SUCCESS":
+            console.log("signout success")
+            return {
+                ...state,
+            }
+        default: 
+            return state
+    }
 }
 
 export default authReducer;
